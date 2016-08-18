fb5137efdd2e3bcd8dd251b81c409f65c1a9a19b
irreversibility <potashnikov.vu@gmail.com>
2016-08-18 20:06:45 +0300

    getData change

--- R/getData.R
+++ R/getData.R
@@ -1,11 +1,15 @@
-getData.scenario <- function(obj, tech = NULL, dem = NULL, sup = NULL,  
-     comm = NULL, group = NULL, region = NULL, year = NULL, slice = NULL, 
-     stg = NULL, expp = NULL, imp = NULL, trade = NULL, variable = NULL, 
+getData.scenario <- function(obj, tech = NA, dem = NA, sup = NA,  
+     comm = NA, group = NA, region = NA, year = NA, slice = NA, 
+     stg = NA, expp = NA, imp = NA, trade = NA, variable = NULL, 
      remove_zero_dim = TRUE, drop = TRUE) {
+  if (is.null(tech) || is.null(dem) || is.null(sup) || is.null(comm)
+    || is.null(group)|| is.null(region) || is.null(year) || is.null(slice)
+    || is.null(stg) || is.null(expp) || is.null(imp) || is.null(trade)) return(NULL);
    #  , type = 'array'
 #  act_set <- c('tech', 'dem', 'sup', 'acomm', 'comm', 'group', 
 #    'region', 'year', 'slice', 'stg', 'expp', 'imp', 'trade')
   dtt <- obj@result@data
+  is.na2 <- function(x) (length(x) == 1 && is.na(x))
   if (!is.null(variable)) {
     if (any(!(variable %in% names(dtt)))) 
       stop(paste('getData: unknown argument: "', paste(variable[!(variable %in% names(dtt))], 
@@ -13,18 +17,18 @@ getData.scenario <- function(obj, tech = NULL, dem = NULL, sup = NULL,
     dtt <- dtt[variable]
   }
   set <- list()
-  if (!is.null(tech)) set[['tech']] <- tech;
-  if (!is.null(dem)) set[['dem']] <- dem;
-  if (!is.null(sup)) set[['sup']] <- sup;
-  if (!is.null(comm)) set[['comm']] <- comm;
-  if (!is.null(group)) set[['group']] <- group;
-  if (!is.null(region)) set[['region']] <- region;
-  if (!is.null(year)) set[['year']] <- year;
-  if (!is.null(slice)) set[['slice']] <- slice;
-  if (!is.null(stg)) set[['stg']] <- stg;
-  if (!is.null(expp)) set[['expp']] <- expp;
-  if (!is.null(imp)) set[['imp']] <- imp;
-  if (!is.null(trade)) set[['trade']] <- trade;
+  if (!is.na2(tech)) set[['tech']] <- tech;
+  if (!is.na2(dem)) set[['dem']] <- dem;
+  if (!is.na2(sup)) set[['sup']] <- sup;
+  if (!is.na2(comm)) set[['comm']] <- comm;
+  if (!is.na2(group)) set[['group']] <- group;
+  if (!is.na2(region)) set[['region']] <- region;
+  if (!is.na2(year)) set[['year']] <- year;
+  if (!is.na2(slice)) set[['slice']] <- slice;
+  if (!is.na2(stg)) set[['stg']] <- stg;
+  if (!is.na2(expp)) set[['expp']] <- expp;
+  if (!is.na2(imp)) set[['imp']] <- imp;
+  if (!is.na2(trade)) set[['trade']] <- trade;
   if (length(set) != 0) {
     for(i in names(set)) {
       rmv <- rep(TRUE, length(dtt))
@@ -47,7 +51,7 @@ getData.scenario <- function(obj, tech = NULL, dem = NULL, sup = NULL,
     }
   }
   if (drop) {
-    for(j in names(dtt)) {
+    for(j in names(dtt)) if (length(dim(dtt[[j]])) > 1) {
       ttt <- paste('dtt[[j]] <- dtt[[j]][', paste(rep(',', length.out = length(dim(dtt[[j]])) - 1), 
        collapse = ''), ', drop = ', drop, ']', sep = '')
       eval(parse(text = ttt))
