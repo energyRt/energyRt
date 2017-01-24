a82ae900736d3f7007064c1886d96b0988af2af7
olugovoy <olugovoy@gmail.com>
2017-01-24 17:51:50 -0500

    version 0.9007 - alpha

--- R/start_msg.R
+++ R/start_msg.R
@@ -1,3 +1,3 @@
 .onAttach <- function(...) {
-  packageStartupMessage(" Loading package: energyRt \n Energy technology modeling toolbox in R \n Authors: Oleg Lugovoy, Vladimir Potashnikov \n Version: 0.00.00.9006-alpha-development (development version from github - for testing only)")
+  packageStartupMessage("\n Loading package: energyRt \n Energy technology modeling toolbox in R \n Authors: Oleg Lugovoy, Vladimir Potashnikov \n Version: 0.00.00.9007-alpha-development (for testing only)")
 }
