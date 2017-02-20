ec393bd8e23518e0a1acb8d9ecc4768cc35b04c7
vpotashnikov <potashnikov.vu@gmail.com>
2017-02-20 22:32:35 +0300

    getData2

--- R/getData.R
+++ R/getData.R
@@ -20,59 +20,52 @@
 #' @param drop
 
 
-getData.scenario <- function(obj, tech = NA, dem = NA, sup = NA,  
-     comm = NA, group = NA, region = NA, year = NA, slice = NA, 
-     stg = NA, expp = NA, imp = NA, trade = NA, variable = NULL, 
-     remove_zero_dim = TRUE, drop = TRUE, DataFrame = FALSE) {
-  if (is.null(tech) || is.null(dem) || is.null(sup) || is.null(comm)
-    || is.null(group)|| is.null(region) || is.null(year) || is.null(slice)
-    || is.null(stg) || is.null(expp) || is.null(imp) || is.null(trade)) return(NULL);
-   #  , type = 'array'
-#  act_set <- c('tech', 'dem', 'sup', 'acomm', 'comm', 'group', 
-#    'region', 'year', 'slice', 'stg', 'expp', 'imp', 'trade')
+#
+
+
+getDataResult0 <- function(obj, ..., variable = NULL, 
+  remove_zero_dim = TRUE, drop = TRUE) {
+  psb_set <- c('tech', 'dem', 'sup', 'comm', 'group', 'region', 
+    'year', 'slice', 'stg', 'expp', 'imp', 'trade')
+  set <- list(...)
+  if (any(!(names(set) %in% psb_set)))
+    stop(paste('Unknown set name "', paste(names(set)[!(names(set) %in% psb_set)], 
+      collapse = '", "'), '"', sep = ''))
   dtt <- obj@result@data
-  is.na2 <- function(x) (length(x) == 1 && is.na(x))
   if (!is.null(variable)) {
     if (any(!(variable %in% names(dtt)))) 
       stop(paste('getData: unknown argument: "', paste(variable[!(variable %in% names(dtt))], 
         collapse = '", "'), '"', sep = ''))
     dtt <- dtt[variable]
   }
-  set <- list()
-  if (!is.na2(tech)) set[['tech']] <- tech;
-  if (!is.na2(dem)) set[['dem']] <- dem;
-  if (!is.na2(sup)) set[['sup']] <- sup;
-  if (!is.na2(comm)) set[['comm']] <- comm;
-  if (!is.na2(group)) set[['group']] <- group;
-  if (!is.na2(region)) set[['region']] <- region;
-  if (!is.na2(year)) set[['year']] <- year;
-  if (!is.na2(slice)) set[['slice']] <- slice;
-  if (!is.na2(stg)) set[['stg']] <- stg;
-  if (!is.na2(expp)) set[['expp']] <- expp;
-  if (!is.na2(imp)) set[['imp']] <- imp;
-  if (!is.na2(trade)) set[['trade']] <- trade;
+  alias_set <- lapply(psb_set, function(x) x)
+  names(alias_set) <- alias_set
+  alias_set$comm = c('comm', 'acomm', 'comme')
+  alias_set$region = c('region', 'regionp', 'src', 'dst')
+  alias_set$year = c('year', 'yearp', 'yeare')
+  alias_set <- alias_set[names(set)]
   if (length(set) != 0) {
     for(i in names(set)) {
       rmv <- rep(TRUE, length(dtt))
       names(rmv) <- names(dtt)
       for(j in names(dtt)) {
-        vv <- grep(paste('^', i, '[[:alpha:]]*', sep = ''), names(dimnames(dtt[[j]])), value = TRUE) 
-        if (i == 'comm' && any(names(dimnames(dtt[[j]])) == 'acomm'))  vv <- c(vv, 'acomm')
-        if (length(vv) > 0 && 
-          any(c(dimnames(dtt[[j]])[vv], recursive = TRUE) %in% set[[i]])) {
-          for(k in vv) if (any(dimnames(dtt[[j]])[[k]] %in% set[[i]])) {
+        if (any(names(dimnames(dtt[[j]])) %in% alias_set[[i]])) {
+          fl <- alias_set[[i]][alias_set[[i]] %in% names(dimnames(dtt[[j]]))]
+          f2 <- lapply(dimnames(dtt[[j]])[fl], function(x) x[x %in% set[[i]]])
+          if (sum(sapply(f2, length) > 0) == 0) rmv[j] <- FALSE else 
+          if (sum(sapply(f2, length) > 0) == 1) {
+            k <- names(f2)[sapply(f2, length) > 0]
             gg <- rep('', length(dim(dtt[[j]])))
             gg[k == names(dimnames(dtt[[j]]))] <- 'dimnames(dtt[[j]])[[k]] %in% set[[i]]'
             ttt <- paste('dtt[[j]] <- dtt[[j]][', paste(gg, collapse = ','), ', drop = ', FALSE, ']', sep = '')
             eval(parse(text = ttt))
-            if (length(dtt[[j]]) == 0) rmv[j] <- FALSE
-          }
-        } else rmv[j] <- FALSE; 
+          } # Nothing to be done if more than one have searching variables
+        }  else rmv[j] <- FALSE; 
       }
       dtt <- dtt[rmv]  
     }
   }
-if (drop) {
+  if (drop) {
     for(j in names(dtt)) if (length(dim(dtt[[j]])) > 0) {
       if (any(dim(dtt[[j]]) != 1)) {
         kk <- names(dimnames(dtt[[j]]))[dim(dtt[[j]]) != 1]
@@ -82,7 +75,6 @@ if (drop) {
         dtt[[j]] <-c(dtt[[j]])
         names(dtt[[j]]) <- NULL
       }
-        
     }
   }
   if (remove_zero_dim && length(dtt) != 0) {
@@ -101,36 +93,13 @@ if (drop) {
           dtt[[j]] <-c(dtt[[j]])
           names(dtt[[j]]) <- NULL
         }
-          
       }
     }
   }
-#  if (type == 'data.frame') {
-#    dff <- list()
-#    for(i in names(dtt)) if (length(dtt[[i]]) != 0) {
-#      dff[[i]] <- as.data.frame.table(dtt[[i]], responseName = 'value', stringsAsFactors = FALSE)
-#    }
-#    if (length(dff) != 0) {
-#      nr <- sum(sapply(dff, nrow))
-#      ncl <- unique(c(lapply(dff, colnames), recursive = TRUE))
-#      dtt <- data.frame(var = character(), stringsAsFactors = FALSE)
-#      for(i in ncl[ncl != 'value']) dtt[[i]] <- character()
-#      dtt[['value']] <- numeric()
-#      dtt[1:nr, ] <- NA
-#      k <- 0
-#      for(i in names(dff)) {
-#        dtt[k + 1:nrow(dff[[i]]), 'var'] <- i
-#        dtt[k + 1:nrow(dff[[i]]), colnames(dff[[i]])] <- dff[[i]]
-#        k <- k + nrow(dff[[i]])
-#      }
-#    } else dtt <- data.frame()
-#  }
-  if(DataFrame) {
-    # as.data.frame.table
-    # dtt@variable <- 
-  } 
   return(dtt)
 }
+
+
 getDataOut <- function(obj, comm) {
   gg <- getData(obj, comm = comm, variable = c("vTechOut", 'vImport', 'vSupOut'), 
     drop = FALSE, type = 'data.frame')
@@ -144,24 +113,157 @@ getDataOut <- function(obj, comm) {
   tapply(gg$value, gg[, c('year', 'src')], sum)
 }
 
-getDataTable <- function(obj, use.dplyr = FALSE, ...) {
-  drop = FALSE
-  lmx <- getData(obj, drop = drop, ...)
-  ltb <- lapply(names(lmx), function(x) {
-    if (is.null(dim(lmx[[x]]))) return(NULL)
-    y <- as.data.frame.table(lmx[[x]], responseName = "value0")
-    y$variable <- as.factor(x)
-    y$value <- y$value0
-    y$value0 <- NULL
-    return(y)
-  })
-  if (use.dplyr) {
-    dft <- Reduce(function(x, y) {dplyr::full_join(x, y)}, ltb)
-    
-  } else {
-    dft <- Reduce(function(x, y) {merge(x, y, all = TRUE)}, ltb)
+getDataParameter <- function(obj, ..., parameter = NULL, 
+  remove_zero_dim = TRUE, drop = TRUE, astable = TRUE, merge.table = FALSE) {
+  psb_set <- c('tech', 'dem', 'sup', 'comm', 'group', 'region', 
+    'year', 'slice', 'stg', 'expp', 'imp', 'trade')
+  set <- list(...)                      
+  if (any(!(names(set) %in% psb_set)))
+    stop(paste('Unknown set name "', paste(names(set)[!(names(set) %in% psb_set)], 
+      collapse = '", "'), '"', sep = ''))
+  fl <- names(obj@precompiled@maptable)[sapply(obj@precompiled@maptable, function(x) 
+    x@type %in% c('single', 'double'))]
+  dtt <- lapply(obj@precompiled@maptable[fl], function(y) getDataMapTable(y))
+  if (!is.null(parameter)) {
+    if (any(!(parameter %in% names(dtt)))) 
+      stop(paste('getDataParameter0: unknown argument: "', paste(parameter[!(parameter %in% names(dtt))], 
+        collapse = '", "'), '"', sep = ''))
+    dtt <- dtt[parameter]
   }
-  return(dft)
+  alias_set <- lapply(psb_set, function(x) x)
+  names(alias_set) <- alias_set
+  alias_set$comm = c('comm', 'acomm', 'comme')
+  alias_set$region = c('region', 'regionp', 'src', 'dst')
+  alias_set$year = c('year', 'yearp', 'yeare')
+  alias_set <- alias_set[names(set)]
+  if (length(set) != 0) {
+    for(i in names(set)) {
+      rmv <- rep(TRUE, length(dtt))
+      names(rmv) <- names(dtt)
+      for(j in names(dtt)) {
+        if (any(colnames(dtt[[j]])[-ncol(dtt[[j]])] %in% alias_set[[i]])) {
+          fl <- alias_set[[i]][alias_set[[i]] %in% colnames(dtt[[j]])[-ncol(dtt[[j]])]]
+          f2 <- sapply(fl, function(x) any(dtt[[j]][, fl] %in% set[[i]]))
+          if (sum(f2) == 0) rmv[j] <- FALSE else 
+          if (sum(f2) == 1) {
+            k <- fl[f2]
+            dtt[[j]] <- dtt[[j]][dtt[[j]][, k] %in% set[[i]],, drop = FALSE]
+          } # Nothing to be done if more than one have searching variables
+        }  else rmv[j] <- FALSE; 
+      }
+      dtt <- dtt[rmv]  
+    }
+  }
+  if (drop) {
+    for(j in names(dtt)) if (ncol(dtt[[j]]) > 1) {
+      dtt[[j]] <- dtt[[j]][, c(apply(dtt[[j]][, -ncol(dtt[[j]]), drop = FALSE], 
+        2, function(x) length(unique(x)) > 1), TRUE), drop = FALSE]
+    }
+  }
+  if (remove_zero_dim && length(dtt) != 0) {
+    dtt <- dtt[sapply(dtt, function(x) any(x[, ncol(x)] != 0))]
+    for(j in names(dtt)) {
+      dtt[[j]] <- dtt[[j]][dtt[[j]][, ncol(dtt[[j]])] == 0,, drop = FALSE]
+    }
+    if (drop) {
+      for(j in names(dtt)) if (ncol(dtt[[j]]) > 1) {
+        dtt[[j]] <- dtt[[j]][, c(apply(dtt[[j]][, -ncol(dtt[[j]]), drop = FALSE], 
+          2, function(x) length(unique(x)) > 1), TRUE), drop = FALSE]
+      }
+    }
+  }
+  dtt <- dtt[sapply(dtt, nrow) > 0]
+  if (!astable) {
+    gg <- names(dtt)
+    dtt <- lapply(dtt, function(x) tapply(x[, ncol(x)], x[, -ncol(x), drop = FALSE], sum))
+    names(dtt) <- gg
+    if (merge.table) {
+      if (use.dplyr) {
+        dtt <- Reduce(function(x, y) {dplyr::full_join(x, y)}, dtt)
+      } else {
+        dtt <- Reduce(function(x, y) {merge(x, y, all = TRUE)}, dtt)
+      }
+    }  
+  } else {
+    for(j in names(dtt)) {
+      colnames(dtt[[j]])[ncol(dtt[[j]])] <- "value"
+    }
+  }
+  return(dtt)
+}
+
+getDataResult <- function(obj, ..., astable = TRUE, use.dplyr = FALSE, merge.table = TRUE) {
+  lmx <- getDataResult0(obj, ...)
+  if (astable) {
+    if (merge.table) {
+      ltb <- lapply(names(lmx), function(x) {
+        if (is.null(dim(lmx[[x]]))) return(NULL)
+        y <- as.data.frame.table(lmx[[x]], responseName = "value0")
+        y$variable <- as.factor(x)
+        y$value <- y$value0
+        y$value0 <- NULL
+        return(y)
+      })
+      names(ltb) <- names(lmx)
+      if (use.dplyr) {
+        dft <- Reduce(function(x, y) {dplyr::full_join(x, y)}, ltb)
+      } else {
+        dft <- Reduce(function(x, y) {merge(x, y, all = TRUE)}, ltb)
+      }
+      dft <- dft[, c(colnames(dft)[colnames(dft) != 'value'], 'value'), drop = FALSE]
+      return(dft)
+    } else {
+      ltb <- lapply(names(lmx), function(x) {
+        if (is.null(dim(lmx[[x]]))) return(NULL)
+        as.data.frame.table(lmx[[x]], responseName = "value")
+      })
+      names(ltb) <- names(lmx)
+      return(ltb)
+    }
+  } else return(lmx)
 } 
 
+
+getData <- function(obj, ..., parameter = NULL, variable = NULL, 
+  get.variable = TRUE, get.parameter = TRUE, merge.table = FALSE,
+  remove_zero_dim = TRUE, drop = TRUE, astable = TRUE, use.dplyr = FALSE) {
+  if (merge.table && !astable) stop('merge is possible only for data.frame')
+  if (get.parameter && get.variable) {
+    if (!merge.table) {
+      l1 <- getDataParameter(obj, ..., parameter = parameter, merge.table = FALSE,
+        remove_zero_dim = remove_zero_dim, drop = drop, astable = astable)
+      l2 <- getDataResult(obj, ..., variable = variable, merge.table = FALSE,
+        remove_zero_dim = remove_zero_dim, drop = drop, astable = astable)
+      for(i in names(l2)) l1[[i]] <- l2[[i]]
+      return(l1)
+    } else {
+      l1 <- getDataParameter(obj, ..., parameter = parameter, merge.table = FALSE,
+        remove_zero_dim = FALSE, drop = FALSE, astable = astable)
+      l2 <- getDataResult(obj, ..., variable = variable, merge.table = FALSE,
+        remove_zero_dim = FALSE, drop = FALSE, astable = astable)
+      for(i in names(l2)) l1[[i]] <- l2[[i]]
+      for(i in names(l1)) l1[[i]]$variable <- i
+      if (use.dplyr) {
+        l1 <- Reduce(function(x, y) {dplyr::full_join(x, y)}, l1)
+      } else {
+        l1 <- Reduce(function(x, y) {merge(x, y, all = TRUE)}, l1)
+      }
+      l1 <- l1[, c(colnames(l1)[colnames(l1) != 'value'], 'value'), drop = FALSE]
+      if (remove_zero_dim) {
+        l1 <- l1[l1[, 'value'] != 0,, drop = FALSE]
+      }
+      if (drop && ncol(l1) > 1) {
+          l1 <- l1[, c(apply(l1[, -ncol(l1), drop = FALSE], 
+            2, function(x) length(unique(x)) > 1), TRUE), drop = FALSE]
+      }
+      return(l1)
+    }
+  } else if (get.parameter) {
+    return(getDataParameter(obj, ..., parameter = parameter, merge.table = merge.table,
+      remove_zero_dim = remove_zero_dim, drop = drop, astable = astable, use.dplyr = use.dplyr)) 
+  } else {
+    return(getDataResult(obj, ..., variable = variable, merge.table = merge.table,
+      remove_zero_dim = remove_zero_dim, drop = drop, astable = astable, use.dplyr = use.dplyr)) 
+  }
+}
   
