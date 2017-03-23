30a47a01580b2be3d859298578d85592ba01a803
olugovoy <olugovoy@gmail.com>
2017-03-23 17:09:27 -0400

    pivot adjustment and variables description in GAMS file updates

--- R/pivot.R
+++ R/pivot.R
@@ -1,28 +1,29 @@
 pivot <- function(scen = NULL, variable = NULL, get_data = FALSE, ...) {
   #df <- arrayhelpers::array2df(scen@result@data[[var]], label.x = var)
   stopifnot(is.logical(get_data))
-  if (class(scen) == "scenario") {scen <- list(scen)}
-  stopifnot(is.list(scen))
-  if (!is.list(variable)) {variable <- as.list(variable)}
-  eg <- expand.grid(1:length(scen), 1:length(variable))
-  lst <- mapply(function(x, y) {
-    dft <- as.data.frame.table(scen[[x]]@result@data[[variable[[y]]]], 
-                               responseName = "value") #[[y]])
-    ii <- dft[, "value"] != 0
-    dft <- dft[ii, ]
-    if (nrow(dft) > 0) {
-      dft$variable <- variable[[y]]
-      dft$scen <- scen[[x]]@name
-    } else {
-      dft$variable <- character()
-      dft$scen <- character()
-    }
-    return(list(dft))
-  },
-  x = eg[, 1], y = eg[, 2])
+  #if (class(scen) == "scenario") {scen <- list(scen)}
+  #stopifnot(is.list(scen))
+  # if (!is.list(variable)) {variable <- as.list(variable)}
+  # eg <- expand.grid(1:length(scen), 1:length(variable))
+  # lst <- mapply(function(x, y) {
+  #   dft <- as.data.frame.table(scen[[x]]@modOut@variables[[variable[[y]]]], 
+  #                              responseName = "value") #[[y]])
+  #   ii <- dft[, "value"] != 0
+  #   dft <- dft[ii, ]
+  #   if (nrow(dft) > 0) {
+  #     dft$variable <- variable[[y]]
+  #     dft$scen <- scen[[x]]@name
+  #   } else {
+  #     dft$variable <- character()
+  #     dft$scen <- character()
+  #   }
+  #   return(list(dft))
+  # },
+  # x = eg[, 1], y = eg[, 2])
   
-  dft <- Reduce(function(x, y, ...) {merge(x, y, all = TRUE)}, lst)
+  #dft <- Reduce(function(x, y, ...) {merge(x, y, all = TRUE)}, lst)
   #dft <- Reduce(function(x, y, ...) {dplyr::full_join(x, y)}, lst)
+  dft <- getData(scen, variable = variable, merge = TRUE, table = TRUE)
   
   if (get_data == F) {
     rpivotTable::rpivotTable(dft,
