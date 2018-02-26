f6fd6735d624d2a9a2d3c00304b427a4cb058c31
vpotashnikov <potashnikov.vu@gmail.com>
2018-02-26 13:59:21 +0300

    still work

--- R/setSlice.R
+++ R/setSlice.R
@@ -4,8 +4,8 @@
 require(energyRt)
 mdl <- new('model')
 # set Slice name vectors
-#.setSlice <- function(mdl, arg, dtf = data.frame(stringsAsFactors = FALSE)) {
- #  arg <- list(...)
+.setSlice <- function(mdl, ...) {
+  arg <- list(...)
   check_colnames <- function(dtf, nm) {
     if (any(colnames(dtf) == nm) || any(grep('^[!A-z]', nm)) || any(gsub('[[:alnum:]]*', nm)))
       stop(paste('Wrong slice level names "', nm, '"', sep = ''))
@@ -27,16 +27,64 @@ mdl <- new('model')
   check_full_slice <- function (dtf) {
     stop('check_full_slice')
   }
-  dtf <- data.frame(stringsAsFactors = FALSE)
   slice_def <- function(dtf, arg) {
     while (length(arg) != 0) {
-      if (is.list(arg[[1]]))
-    }
-      dtf <- check_colnames(dtf, arg[[1]])
-      if ()
-        dtf <- .setSlice
+      if (!(is.character(arg[[1]]) || (!is.null(names(arg)) && !is.na(names(arg))[1]) && 
+            is.list(arg[[1]])))
+        stop(paste('.setSlice: Unknown argument: ', paste(capture.output(print(arg[[1]])), collapse = '\n'),
+                   sep = '\n'))
+      if (is.character(arg[[1]])) {
+        lv <- arg[[1]];
+        arg <- arg[-1]
+      } else {
+        lv <- names(arg)[1]
+      }
+      if (length(arg) == 0) 
+        stop(paste('.setSlice: There are no slice for level "', lv, '"', sep = ''))
+      dtf[, lv] <- rep(character(), nrow(dtf))
+      if (is.character(arg[[1]]) || (!is.null(names(arg[[1]])) && is.numeric(arg[[1]]))) {
+        if (is.character(arg[[1]])) {
+          val_sh <- rep(1 / length(arg[[1]]), length(arg[[1]]))
+          val_nm <- arg[[1]]
+        } else {
+          val_sh <- arg[[1]]
+          val_nm <- names(arg[[1]])
+        }
+        check_slice(val_nm)
+        if (any(val_nm <= 0) || sum(val_sh) != 1)
+          stop(paste(paste('.setSlice: There are wronf slice data for level "', lv, '"\n', 
+                           sep = ''), paste(capture.output(print(arg[[1]])), collapse = '\n'), sep = '\n'))
+        arg <- arg[-1]
+        dtf0 <- dtf; dtf <- dtf[0,, drop = FALSE];
+        dtf[, lv] <- rep(character(), nrow(dtf))
+        if (nrow(dtf0) != 0) {
+          dtf[1:(nrow(dtf0) * length(val_sh)), ] <- NA
+          for (i in 2:ncol(dtf0)) dtf[, i] <- dtf0[, i]
+          dtf[, lv] <- c(t(matrix(val_nm, length(val_sh), nrow(dtf0))))
+          dtf[, 'share'] <- (dtf0[, 'share'] * c(t(matrix(val_sh, length(val_sh), nrow(dtf0)))))
+        } else {
+          dtf[1:length(val_sh), ] <- NA
+          dtf[, lv] <- val_nm
+          dtf[, 'share'] <- val_sh
+        }
+      } else if (is.list(arg[[1]])) {
+        dtf0 <- dtf
+        while (length(arg) != 0) 
+          dtf <- rbind(dtf, slice_def(dtf, arg[[1]]))
+        arg <- arg[-1]
+      } else stop(paste('.setSlice: Unknown type of argument for slice level "', lv, '"', sep = ''))
     }
     dtf
   }
-  
-#}
+  dtf <- data.frame(share = numeric(), stringsAsFactors = FALSE)
+  dtf <- slice_def(dtf, arg)
+  dtf[, c(2:ncol(dtf), 1), drop = FALSE]
+}
+
+arg <- list('lv', paste('x', 1:12, sep = ''))
+
+.setSlice(mdl, 'lv', paste('x', 1:12, sep = ''))
+
+.setSlice(mdl, lv1 = list(c('hh' = .5, 'gg' = .25, 'll' = .25)))
+
+
