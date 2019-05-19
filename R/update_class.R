99470dd8b2aa74cd13fb6d2dafd0e57b36e25bae
vpotashnikov <potashnikov.vu@gmail.com>
2019-05-19 18:21:54 +0300

    update for technology, supply, demand & storage work

--- R/update_class.R
+++ R/update_class.R
@@ -1,6 +1,6 @@
 .remove_all_par_from_param <- function(scen, lst) {
   cls <- class(lst[[1]])
-  slc <- c(technology = 'tech', supply = 'sup', storage = 'stg')[cls]
+  slc <- c(technology = 'tech', supply = 'sup', storage = 'stg', demand = 'dem')[cls]
   all_par <- grep('^(p|m)Cns', names(scen@modInp@parameters), value = TRUE, invert = TRUE)
   tec_name <- sapply(lst, function(x) x@name)
   # Remove previous data
@@ -16,12 +16,12 @@
     }
   }
   # Add change technology
-  for(i in tec_name) {
+  for(i in seq_along(tec_name)) {
     scen@modInp <- add_name(scen@modInp, lst[[i]], scen@misc$approxim)
   }
   scen@modInp@set <- lapply(scen@modInp@parameters[sapply(scen@modInp@parameters, function(x) x@type == 'set')], function(x) getParameterData(x)[, 1])
-  for(i in tec_name) {
-    scen@modInp <- .add0(scen@modInp, arg$technology[[i]], approxim = scen@misc$approxim)
+  for(i in seq_along(tec_name)) {
+    scen@modInp <- energyRt:::.add0(scen@modInp, lst[[i]], approxim = scen@misc$approxim)
   }
   scen
 }
@@ -29,22 +29,39 @@
   p1 = proc.time()[3];
   cat('Update model ')
   arg <- list(...)
+  if (length(arg) == 1 && class(arg[[1]]) == 'list') {
+    arg <- arg[[1]]
+  }
   cls <- sapply(arg, class)
   arg <- lapply(unique(cls), function(x) arg[x == cls])  
   names(arg) <- unique(cls)
-  if (!is.null(arg$technology)) {
-    scen <- .remove_all_par_from_param(scen, arg$technology)
+  for (i in c('technology', 'supply', 'storage', 'demand'))
+    if (!is.null(arg[[i]])) {
+      scen <- .remove_all_par_from_param(scen, arg[[i]])
   }
-  if (!is.null(arg$supply)) {
-    scen <- .remove_all_par_from_param(scen, arg$supply)
-  }
-  if (!is.null(arg$storage)) {
-    scen <- .remove_all_par_from_param(scen, arg$storage)
+  # Clean
+  # scen@modInp <- .add0(scen@modInp, scen@model@sysInfo, approxim = scen@misc$approxim) 
+  # Reduce mapping
+  reduce_map <- c('mTechInpTot',  'mTechOutTot',  'mSupOutTot',  'mDemInp',  'mTechEmsFuel',  'mEmsFuelTot',  
+                   'mDummyImport',  'mDummyExport',  'mDummyCost',  'mTradeIr',  'mTradeIrUp',  'mTradeIrAInp2',  'mTradeIrAInpTot',  
+                   'mTradeIrAOut2',  'mTradeIrAOutTot',  'mImportRow',  'mImportRowUp',  'mImportRowAccumulatedUp',  'mExportRow',  'mExportRowUp',  
+                   'mExportRowAccumulatedUp',  'mExport',  'mImport',  'mStorageInpTot',  'mStorageOutTot',  'mTaxCost',  'mSubsCost',  'mAggOut',  'mSupAva',  
+                   'mSupAvaUp',  'mSupReserveUp',  'mTechAfUp',  'mTechAfcUp',  'mTechOlifeInf',  'mStorageOlifeInf',  'mOut2Lo',  'mInp2Lo')
+  for (i in reduce_map) {
+    scen@modInp@parameters[[i]]@data <- scen@modInp@parameters[[i]]@data[0,, drop = FALSE]
+    if (scen@modInp@parameters[[i]]@nValues != -1) 
+      scen@modInp@parameters[[i]]@nValues <- 0
   }
   scen@modInp <- energyRt:::.reduce_mapping(scen@modInp)
+  # Clean parameters, need when nValues != -1, and mean that add NA row for speed
+  for(i in names(scen@modInp@parameters)) {
+    if (scen@modInp@parameters[[i]]@nValues != -1) {
+      scen@modInp@parameters[[i]]@data <- scen@modInp@parameters[[i]]@data[
+        seq(length.out = scen@modInp@parameters[[i]]@nValues),, drop = FALSE]
+    }
+  }
   cat(round(proc.time()[3] - p1, 2), 's\n', sep = '')
   scen
 }
 
-# scen = BAU
-# arg = list(mdl@data[[1]]@data$ELC_Coallz)
+# scen = BAU; arg = list(Build_250_2030)
