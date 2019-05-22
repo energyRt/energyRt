bb23210e5feaeb1a76bfbef79bc8b3652fd926dd
vpotashnikov <potashnikov.vu@gmail.com>
2019-05-22 20:12:33 +0300

    Update update_class.R

--- R/update_class.R
+++ R/update_class.R
@@ -1,6 +1,6 @@
 .replace_tech_sup_stg_dem <- function(scen, lst) {
   cls <- class(lst[[1]])
-  slc <- c(technology = 'tech', supply = 'sup', storage = 'stg', demand = 'dem')[cls]
+  slc <- c(technology = 'tech', supply = 'sup', storage = 'stg', demand = 'dem', export = 'expp', import = 'imp')[cls]
   all_par <- grep('^(p|m)Cns', names(scen@modInp@parameters), value = TRUE, invert = TRUE)
   tec_name <- sapply(lst, function(x) x@name)
   # Remove previous data
@@ -25,6 +25,21 @@
   }
   scen
 }
+
+.replace_comm <- function(scen, lst) {
+	# May only add commodity, not remove
+	cls <- class(lst[[1]])
+	# Add change technology
+	for(i in seq_along(lst)) {
+		scen@modInp <- add_name(scen@modInp, lst[[i]], scen@misc$approxim)
+	}
+	scen@modInp@set <- lapply(scen@modInp@parameters[sapply(scen@modInp@parameters, function(x) x@type == 'set')], function(x) getParameterData(x)[, 1])
+	for(i in seq_along(lst)) {
+		scen@modInp <- energyRt:::.add0(scen@modInp, lst[[i]], approxim = scen@misc$approxim)
+	}
+	scen
+}
+
 
 .replace_taxsub <- function(scen, lst) {
   cls <- class(lst[[1]])
@@ -72,7 +87,8 @@
     arg <- arg[[1]]
   }
   cls <- sapply(arg, class)
-  not_rel <- cls[!(cls %in%c('technology', 'supply', 'storage', 'demand', 'tax', 'sub', 'constraint', 'sysInfo'))]
+  not_rel <- cls[!(cls %in%c('technology', 'supply', 'storage', 'demand', 'tax', 'sub', 
+  	'constraint', 'sysInfo', 'commodity', 'export', 'import'))]
   if (length(not_rel))
     stop(paste0('Not relised class for "', paste0(not_rel, collapse = '", "'), '"'))
   # Replace in model
@@ -81,7 +97,7 @@
   #Replace in parameters
   arg <- lapply(unique(cls), function(x) arg[x == cls])  
   names(arg) <- unique(cls)
-  for (i in c('technology', 'supply', 'storage', 'demand'))
+  for (i in c('technology', 'supply', 'storage', 'demand', 'export', 'import'))
     if (!is.null(arg[[i]])) {
       scen <- .replace_tech_sup_stg_dem(scen, arg[[i]])
     }
@@ -91,6 +107,8 @@
     }
   if (!is.null(arg$constraint)) 
   	scen <- .replace_constraint(scen, arg$constraint)
+  if (!is.null(arg$commodity)) 
+  	scen <- .replace_comm(scen, arg$commodity)
   if (!is.null(arg$sysInfo))
   	scen@model@sysInfo <- arg$sysInfo[[1]]
   # Clean
