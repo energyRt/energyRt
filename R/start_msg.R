40e6b850fc3742fa29951c9fb65e4520c035d0f0
olugovoy <olugovoy@gmail.com>
2019-09-19 08:17:38 -0600

    version update

--- R/start_msg.R
+++ R/start_msg.R
@@ -1,4 +1,4 @@
 .onAttach <- function(...) {
-  packageStartupMessage("\n Loading package: energyRt \n Energy technology modeling toolbox in R \n Version: 0.01.01.9000-beta (development version from GitHub, for testing only) \n  <http://www.energyrt.org> \n ")
+  packageStartupMessage("\n Loading package: energyRt \n Energy technology modeling toolbox in R \n Version: 0.01.02.9000-beta (development version from GitHub, for testing only) \n  <http://www.energyrt.org> \n ")
 }
  
