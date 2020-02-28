c7a635bbfd1dfd580fe92d404a7e2d31e0a8c38c
vpotashnikov <potashnikov.vu@gmail.com>
2020-02-28 17:20:15 +0300

    Update checkDuplication.R

--- R/checkDuplication.R
+++ R/checkDuplication.R
@@ -1,5 +1,23 @@
 checkDuplication <- function(x) {
   checkDuplication0 <- function(x) {
+    check_by_slots <- function(x, slt_name) {
+      rs <- NULL
+      for (i in slt_name) {
+        slt <- slot(x, i)
+        set_slot <- colnames(slt)[colnames(slt) %in% energyRt:::.set_al]
+        value_slot <- colnames(slt)[!(colnames(slt) %in% set_slot)]
+        fl <- !is.na(slt[, value_slot, drop = FALSE])
+        if (any(fl)) {
+          for (j in value_slot[apply(fl, 2, any)]) {
+            f2 <- duplicated(slt[fl[, j], set_slot, drop = FALSE])
+            if (any(f2)) {
+              rs <- rbind(rs, data.frame(slot = i, parameter = j, value = sum(f2), stringsAsFactors = FALSE))
+            }
+          }
+        }
+      }
+      return(rs)
+    }
     if (class(x) == 'scenario') 
       return(checkDuplication0(x@model))
     res <- data.frame(repository = character(), object = character(), slot = character(), parameter = character(), stringsAsFactors = FALSE)
@@ -11,6 +29,11 @@ checkDuplication <- function(x) {
           tmp$repository <- x@data[[i]]@name
           rs <- rbind(rs, tmp)
         }
+      }
+      tmp <- checkDuplication0(x@sysInfo)
+      if (!is.null(tmp)) {
+        tmp$repository <- 'slot::sysInfo'
+        rs <- rbind(rs, tmp)
       }
       if (is.null(rs)) return(NULL)
       return(rs[, c(ncol(rs), 1:(ncol(rs) - 1))])
@@ -30,23 +53,10 @@ checkDuplication <- function(x) {
     if (class(x) %in% c('tax', 'sub', 'weather', 'supply', 'import', 'export', 'trade', 'technology', 'demand')) {
       slt_name <- getSlots(class(x))
       slt_name <- names(slt_name)[slt_name == 'data.frame' & !(names(slt_name) %in% c('input', 'output', 'aux'))]
-      rs <- NULL
-      for (i in slt_name) {
-        slt <- slot(x, i)
-        set_slot <- colnames(slt)[colnames(slt) %in% energyRt:::.set_al]
-        value_slot <- colnames(slt)[!(colnames(slt) %in% set_slot)]
-        fl <- !is.na(slt[, value_slot, drop = FALSE])
-        if (any(fl)) {
-          for (j in value_slot[apply(fl, 2, any)]) {
-            f2 <- duplicated(slt[fl[, j], set_slot, drop = FALSE])
-            if (any(f2)) {
-              rs <- rbind(rs, data.frame(slot = i, parameter = j, value = sum(f2), stringsAsFactors = FALSE))
-            }
-          }
-        }
-      }
-      return(rs)
+      return(check_by_slots(x, slt_name))
     } else if (class(x) %in% c('slice', 'constraint', 'commodity')) {
+    } else if (class(x) %in% c('sysInfo')) {
+      return(check_by_slots(x, c('debug', 'discount')))
     } else warning(paste0('Unknown class "', class(x), '"'))
     NULL
   }
