4924240c6e10cdfa58f3acd84745ec13bd613851
vpotashnikov <potashnikov.vu@gmail.com>
2020-04-03 14:26:09 +0300

    Update write_model_GAMS.R

--- R/write_model_GAMS.R
+++ R/write_model_GAMS.R
@@ -31,20 +31,7 @@
         cat(paste0('$include input/', i, '.gms\n'), file = zz_data_gms)
       }
     }    
-    tlp <- lapply(0:(arg$n.threads - 1), function(y) names(scen@modInp@parameters)[
-      seq_along(scen@modInp@parameters) %% arg$n.threads == y])
-    cl <- makeCluster(arg$n.threads)
-    wrt_fun <- function(x, tlp, par, drr) {
-      require(energyRt)
-      for (i in tlp[[x + 1]]) {
-        zz_data_tmp <- file(paste(drr, '/input/', i, '.gms', sep = ''), 'w')
-        cat(.toGams(par[[i]]), sep = '\n', file = zz_data_tmp)
-        close(zz_data_tmp)
-      }
-      NULL
-    }
-    parLapply(cl, 0:(arg$n.threads - 1), wrt_fun, tlp, scen@modInp@parameters, arg$dir.result)
-    stopCluster(cl)
+    .write_multi_threads(arg, scen, func = .toGams, type = 'gms')
   }
   close(zz_data_gms)    
   ### Model code to text
@@ -98,5 +85,24 @@
   scen
 }
 
+
+.write_multi_threads <- function(arg, scen, func, type) {
+  require(parallel)
+  tlp <- lapply(0:(arg$n.threads - 1), function(y) names(scen@modInp@parameters)[
+    seq_along(scen@modInp@parameters) %% arg$n.threads == y])
+  cl <- makeCluster(arg$n.threads)
+  wrt_fun <- function(x, tlp, par, drr, func, type) {
+    require(energyRt)
+    for (i in tlp[[x + 1]]) {
+      zz_data_tmp <- file(paste(drr, '/input/', i, '.', type, sep = ''), 'w')
+      cat(func(par[[i]]), sep = '\n', file = zz_data_tmp)
+      close(zz_data_tmp)
+    }
+    NULL
+  }
+  parLapply(cl, 0:(arg$n.threads - 1), wrt_fun, tlp, scen@modInp@parameters, arg$dir.result, func, type)
+  stopCluster(cl)
+  NULL
+}
 
 
