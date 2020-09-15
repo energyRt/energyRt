9ca289c8d072a90015577a1ec7be00ff341cae89
olugovoy <olugovoy@gmail.com>
2020-09-15 08:38:06 -0400

    version change for pulling

--- R/start_msg.R
+++ R/start_msg.R
@@ -1,4 +1,4 @@
 .onAttach <- function(...) {
-  packageStartupMessage("\n Loading package: energyRt \n Energy technology modeling toolbox in R \n Version: 0.01.12.9000-beta (development version from GitHub) \n  <http://www.energyrt.org> \n ")
+  packageStartupMessage("\n Loading package: energyRt \n Energy technology modeling toolbox in R \n Version: 0.01.13-dev (development version from GitHub) \n  <http://www.energyrt.org> \n ")
 }
  
