959582563f2bef9dbcf3c04c1cc88999516a9f94
vpotashnikov <potashnikov.vu@gmail.com>
2020-09-18 23:20:58 +0300

    Update y_add_trade.R

--- R/y_add_trade.R
+++ R/y_add_trade.R
@@ -198,7 +198,8 @@ setMethod('.add0', signature(obj = 'modInp', app = 'trade',
 				}
 			}
 			invcost <- simpleInterpolation(trd@invcost, 'invcost', obj@parameters[['pTradeInvcost']], approxim, 'trade', trd@name)
-			invcost <- invcost[invcost$value != 0 & trd@start <= invcost$year & invcost$year <= trd@end,, drop = FALSE]
+			invcost <- invcost[invcost$value != 0,, drop = FALSE]
+			if (!is.null(invcost$year)) invcost <- invcost[trd@start <= invcost$year & invcost$year <= trd@end,, drop = FALSE]
 			if (nrow(invcost) == 0) invcost <- NULL
 			stock_exist <- simpleInterpolation(trd@stock, 'stock', obj@parameters[['pTradeStock']], approxim, 'trade', trd@name)
 			obj@parameters[['pTradeStock']] <- addData(obj@parameters[['pTradeStock']], stock_exist)
@@ -236,6 +237,14 @@ setMethod('.add0', signature(obj = 'modInp', app = 'trade',
 			if (!is.null(invcost)) {
 				end_year <- max(approxim$year)
 				obj@parameters[['pTradeInvcost']] <- addData(obj@parameters[['pTradeInvcost']], invcost)
+				if (any(!(obj@parameters[['mTradeInv']]@dimSetNames %in% colnames(invcost)))) {
+					if (is.null(invcost$year)) {
+						invcost <- merge(invcost, list(year = possible_invest_year))
+					}
+					if (is.null(invcost$region)) {
+						invcost <- merge(invcost, approxim['region'])
+					}
+				}
 				obj@parameters[['mTradeInv']] <- addData(obj@parameters[['mTradeInv']], invcost[, colnames(invcost) != 'value'])
 				invcost$invcost <- invcost$value; invcost$value <- NULL
 				if (length(trade_eac) > 0) {
