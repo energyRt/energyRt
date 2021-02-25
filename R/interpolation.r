26a3aa0f8770f11e4f1326629ee8205c45434246
vpotashnikov <potashnikov.vu@gmail.com>
2021-02-25 19:50:28 +0300

    interpolation.r & smInterpolation.r was fixed

--- R/interpolation.r
+++ R/interpolation.r
@@ -28,11 +28,11 @@
   true_prior <- true_prior[true_prior %in% prior]
   approxim <- approxim[names(approxim) %in% prior]
   # Remove excess column
-  obj <- obj[,colnames(obj) %in% c(prior, parameter), drop = FALSE]
+  obj <- obj[,colnames(obj) %in% c(prior, parameter), with = FALSE]
   # Sort column
   obj <- obj[,c(prior[prior %in% colnames(obj)], 
-    colnames(obj)[ncol(obj)]), drop = FALSE]
-  obj <- obj[!is.na(obj[, parameter]), , drop = FALSE]
+    colnames(obj)[ncol(obj)]), with = FALSE]
+  obj <- obj[!is.na(obj[[parameter]]),]
   if (anyDuplicated(obj[, -ncol(obj)])) {
     jjj <- sys.status()
     kkk <- sapply(jjj$sys.calls, function(x) any(grep('.add0', x[1])))
@@ -58,13 +58,13 @@
     if (is.null(approxim2$year))
       approxim2$year <- arg$approxim$year
   }
-  tmp_nona <- (!is.na(obj[, -ncol(obj), drop = FALSE]))
+  tmp_nona <- (!is.na(obj[, -ncol(obj)]))
   if (all(tmp_nona)) { # There is not NA column
     possible_comb <- prod(sapply(approxim2, length))
     if (nrow(obj) >= possible_comb) {
      obj3 <- obj
      for (i in names(approxim2))
-        obj3 <- obj3[obj3[[i]] %in% approxim2[[i]],, drop = FALSE]
+        obj3 <- obj3[obj3[[i]] %in% approxim2[[i]],]
       if (nrow(obj3) == possible_comb)
         return(obj3)
     }
@@ -74,12 +74,12 @@
     if (all(f1 == f2)) { # Could be small appr
       obj2 <- obj[, c(f1, TRUE), drop = FALSE]
       for (i in colnames(obj2)[-ncol(obj2)])
-        obj2 <- obj2[obj2[[i]] %in% approxim2[[i]],, drop = FALSE]
+        obj2 <- obj2[obj2[[i]] %in% approxim2[[i]],]
       if (ncol(obj2) == 1 || nrow(obj2) == prod(sapply(approxim2[names(obj2)[-ncol(obj2)]], length))) { # Simple approximation is possible
         for (i in names(obj)[c(!f1, FALSE)]) {
           obj2 <- merge(obj2, approxim2[i])
         }
-        return(obj2[, colnames(obj)])
+        return(obj2[[colnames(obj)]])
       }
     }
   }
@@ -91,31 +91,31 @@
     approxim$year <- yy[1]:yy[2]
     apr <- approxim[c('year', true_prior[true_prior != 'year'])]
     if (any(sapply(apr, length) == 0)) return(NULL)
-    dd <- as.data.table.table(array(NA, dim = sapply(apr, length), 
-      dimnames = apr), stringsAsFactors = FALSE, responseName = parameter)
-    dd <- dd[, c(prior, parameter), drop = FALSE]
+    dd <- as.data.table(as.data.frame.table(array(numeric(), dim = sapply(apr, length), 
+      dimnames = apr), stringsAsFactors = FALSE, responseName = parameter))
+    dd <- dd[, c(prior, parameter), with = FALSE]
   } else {
-    dd <- as.data.table.table(array(NA, dim = sapply(approxim, length), 
-      dimnames = approxim), stringsAsFactors = FALSE, responseName = parameter)
+    dd <- as.data.table(as.data.frame.table(array(numeric(), dim = sapply(approxim, length), 
+      dimnames = approxim), stringsAsFactors = FALSE, responseName = parameter))
   }
   if (nrow(obj) != 0) {
       ii <- 2 ^ (seq(length.out = ncol(obj) - 1) - 1)
-      KK <- colSums(ii * t(is.na(obj[, true_prior[true_prior %in% prior], drop = FALSE])))
-      dobj <- as.matrix(obj[, -ncol(obj), drop = FALSE])
-      ddd <- t(as.matrix(dd[, -ncol(dd), drop = FALSE]))
-      dff <- dd[, -ncol(dd), drop = FALSE]
-      obj <- obj[, c(colnames(dff), parameter), drop = FALSE]
-      for(i in 1:ncol(dff)) dff[, i] <- as.factor(as.character(dff[, i]))
-      for(i in 1:ncol(dff)) obj[, i] <- factor(as.character(obj[[i]]), levels = levels(dff[, i]))
-      for(i in 1:ncol(dff)) obj[, i] <- as.numeric(obj[[i]])
-      for(i in 1:ncol(dff)) dff[, i] <- as.numeric(dff[, i])
+      KK <- colSums(ii * t(is.na(obj[, true_prior[true_prior %in% prior], with = FALSE])))
+      dobj <- as.matrix(obj[, -ncol(obj)])
+      ddd <- t(as.matrix(dd[, -ncol(dd)]))
+      dff <- dd[, -ncol(dd)]
+      obj <- obj[, c(colnames(dff), parameter), with = FALSE]
+      for(i in 1:ncol(dff)) dff[[i]] <- as.factor(as.character(dff[[i]]))
+      for(i in 1:ncol(dff)) obj[[i]] <- factor(as.character(obj[[i]]), levels = levels(dff[[i]]))
+      for(i in 1:ncol(dff)) obj[[i]] <- as.numeric(obj[[i]])
+      for(i in 1:ncol(dff)) dff[[i]] <- as.numeric(dff[[i]])
       hh <- sapply(dff, max)
       hh <- c(1, cumprod(hh[-length(hh)]))
       dff <- as.matrix(dff)
       obj <- as.matrix(obj)
       for (i in 1:ncol(dff)) {
-        dff[, i] <- hh[i] * (dff[, i] - 1)
-        obj[, i] <- hh[i] * (obj[, i] - 1)
+        dff[[i]] <- hh[i] * (dff[[i]] - 1)
+        obj[[i]] <- hh[i] * (obj[[i]] - 1)
       }
       # check all(sort(rowSums(dff)) == 0:max(rowSums(dff)))
       for(i in rev(sort(unique(KK)))) {
@@ -123,25 +123,25 @@
         #dff <- dd[fl, -ncol(dd), drop = FALSE]
         zz <- !is.na(obj[fl[1], -ncol(obj)])
         # gg <- rowSums(obj[fl, -ncol(obj), drop = FALSE])
-        r1 <- rowSums(dff[, zz, drop = FALSE])
-        r2 <- rowSums(obj[fl, c(zz, FALSE), drop = FALSE])
-        ll <- obj[fl, ncol(obj)]
+        r1 <- rowSums(dff[, zz, with = FALSE])
+        r2 <- rowSums(obj[fl, c(zz, FALSE), with = FALSE])
+        ll <- obj[[ncol(obj)]][fl]
         names(ll) <- r2
         nn <- (r1 %in% r2)
-        dd[nn, ncol(dd)] <- ll[as.character(r1[nn])]
+        dd[[ncol(dd)]][nn] <- ll[as.character(r1[nn])]
       }
   } 
   # Interpolation
   if (!there.are.year) {
-    dd[is.na(dd[, parameter]), parameter] <- defVal
+    dd[is.na(dd[[parameter]]), parameter] <- defVal
   } else {
-    if (all(is.na(dd[, parameter]))) {
-      dd[is.na(dd[, parameter]), parameter] <- defVal
+    if (all(is.na(dd[[parameter]]))) {
+      dd[is.na(dd[[parameter]]), parameter] <- defVal
     } else if (any(is.na(dd[, parameter]))) {
-      zz <- matrix(dd[, parameter], length(approxim$year))
+      zz <- matrix(dd[[parameter]], length(approxim$year))
       f1 <- apply(!is.na(zz), 2, all)
       if (any(!f1)) {
-        gg <- seq(along =f1)[!f1][apply(is.na(zz[, !f1, drop = FALSE]), 2, all)]
+        gg <- seq(along = f1)[!f1][apply(is.na(zz[, !f1, drop = FALSE]), 2, all)]
         zz[, gg] <- defVal
         f1[gg] <- TRUE
       }
@@ -185,11 +185,11 @@
       dd[, parameter] <- c(zz)  
     }
     if (any(colnames(obj)[-ncol(obj)] == 'slice')) {
-      dd <- dd[, c(true_prior, parameter), drop = FALSE]
+      dd <- dd[, c(true_prior, parameter), with = FALSE]
     }
     if (length(approxim$year) != year_range[2] - year_range[1] + 1) {
       dd <- dd[rep(year_range[1] <= approxim$year & approxim$year <= year_range[2], 
-        nrow(dd) / length(approxim$year)), , drop = FALSE]
+        nrow(dd) / length(approxim$year)),]
     }
   }
   return(dd)
@@ -217,23 +217,24 @@
 #   parameter = 'character', defVal = 'numeric', rule = 'character'), 
 .interpolation_bound <-  function(obj, parameter, defVal, rule, ...) {
   gg <- paste(parameter, c('.lo', '.fx', '.up'), sep = '')
-  aa <- obj[, !(colnames(obj) %in% gg), drop = FALSE]; aa[, parameter] <- rep(NA, nrow(aa))
-  a1 <- aa; a1[, parameter] <- obj[, gg[1]]
-  a2 <- aa; a2[, parameter] <- obj[, gg[2]]
-  a3 <- aa; a3[, parameter] <- obj[, gg[3]]
+  aa <- obj[, !(colnames(obj) %in% gg), with = FALSE]; 
+  aa[, parameter, with = FALSE] <- rep(NA, nrow(aa))
+  a1 <- aa; a1[, parameter, with = FALSE] <- obj[, gg[1], with = FALSE]
+  a2 <- aa; a2[, parameter, with = FALSE] <- obj[, gg[2], with = FALSE]
+  a3 <- aa; a3[, parameter, with = FALSE] <- obj[, gg[3], with = FALSE]
   d1 <- .interpolation(rbind(a1, a2), parameter, 
       defVal = defVal[1], rule = rule[1], ...)
   if (!is.null(d1)) {
-    dd <- d1[, -ncol(d1), drop = FALSE]
-    dd[, 'type'] <- 'lo'
-    dd[, parameter] <- d1[, parameter]
+    dd <- d1[, -ncol(d1), with = FALSE]
+    dd[, 'type', with = FALSE] <- 'lo'
+    dd[, parameter, with = FALSE] <- d1[, parameter, with = FALSE]
     }
   d2 <- .interpolation(rbind(a3, a2), parameter, 
     defVal = defVal[2], rule = rule[2], ...)
   if (!is.null(d2)) {
-    zz <- d2[, -ncol(d2), drop = FALSE]
-    zz[, 'type'] <- 'up'
-    zz[, parameter] <- d2[, parameter]
+    zz <- d2[, -ncol(d2), with = FALSE]
+    zz[, 'type', with = FALSE] <- 'up'
+    zz[, parameter, with = FALSE] <- d2[, parameter, with = FALSE]
   }
   if (!is.null(d1) && !is.null(d2)) {
     return(rbind(dd, zz))
--- R/smInterpolation.r
+++ R/smInterpolation.r
@@ -9,14 +9,14 @@ simpleInterpolation <- function(frm, parameter, mtp, approxim,
   
   if (!is.null(mtp@misc$not_need_interpolate)) {
     # approxim <- approxim[!(names(approxim) %in% mtp@misc$not_need_interpolate)]
-    frm <- frm[, !(colnames(frm) %in% mtp@misc$not_need_interpolate), drop = FALSE]
+    frm <- frm[, !(colnames(frm) %in% mtp@misc$not_need_interpolate), with = FALSE]
     if (any(mtp@misc$not_need_interpolate == 'year')) there.is.year <- FALSE
     fl <- add_set_name %in% mtp@misc$not_need_interpolate
     if (any(fl)) {
       add_set_name <- add_set_name[!fl]
       add_set_value <- add_set_value[!fl]
     }
-    frm <- frm[!duplicated(frm),, drop = FALSE]
+    frm <- frm[!duplicated(frm),]
   }
 
   dd <- .interpolation(frm, parameter,
@@ -24,10 +24,9 @@ simpleInterpolation <- function(frm, parameter, mtp, approxim,
                     defVal    = mtp@defVal,
                     year_range = range(approxim$year),
                     approxim   = approxim, all = all.val)
-
   if (is.null(dd)) return(NULL)
   if (!all.val) {
-    dd <- dd[dd[[ncol(dd)]] != 0,, drop = FALSE]
+    dd <- dd[dd[[ncol(dd)]] != 0,]
     if (nrow(dd) == 0) return(NULL)
   }
   # Must fixed in the future
@@ -37,11 +36,11 @@ simpleInterpolation <- function(frm, parameter, mtp, approxim,
   }
   if (there.is.year) dd[['year']] <- as.numeric(dd[['year']])
   if (is.null(add_set_name)) {
-    dd <- dd[, c(mtp@dimSetNames, 'value'), drop = FALSE]
+    dd <- dd[, c(mtp@dimSetNames, 'value'), with = FALSE]
   } else {
     d3 <- data.table()
     for(i in 1:length(add_set_value))
-        d3[1:nrow(dd), i] <- rep(add_set_value[i])
+        d3[1:nrow(dd), i] <- add_set_value[i]
     colnames(d3) <- add_set_name
     stnd <- mtp@dimSetNames[-(1:length(d3))]
     # It was added for trading routes
@@ -49,18 +48,17 @@ simpleInterpolation <- function(frm, parameter, mtp, approxim,
     	stnd <- c(stnd[stnd != 'src' & stnd != 'dst'], 'region')
     } 
     stnd <- stnd[!(stnd %in% mtp@misc$not_need_interpolate)]
-    if (any(ls(globalenv()) == 'KKK')) browser()
-    dd <- cbind(d3, dd[, c(stnd, 'value'), drop = FALSE])
+    dd <- cbind(d3, dd[, c(stnd, 'value'), with = FALSE])
   }
   if (!is.null(remove_duplicate) && nrow(dd) != 0) {
     fl <- rep(TRUE, nrow(dd))
     for (i in seq_along(remove_duplicate)) {
-      fl <- (fl & dd[, remove_duplicate[[i]][1]] != dd[, remove_duplicate[[i]][2]])
+      fl <- (fl & dd[, remove_duplicate[[i]][1], with = FALSE] != dd[, remove_duplicate[[i]][2], with = FALSE])
     }
-    dd <- dd[fl,, drop = FALSE]
+    dd <- dd[fl,]
   }
   if (there.is.year && !is.null(approxim$mileStoneYears)) {
-    dd <- dd[dd$year %in% approxim$mileStoneYears,, drop = FALSE]
+    dd <- dd[dd$year %in% approxim$mileStoneYears,]
   }
   if (nrow(dd) == 0) return(NULL)
   
@@ -72,7 +70,7 @@ multiInterpolation <- function(frm, parameter, mtp, approxim,
   remValueUp = NULL, remValueLo = NULL) {
   there.is.year <- any(colnames(frm) == 'year')
   if (!is.null(mtp@misc$not_need_interpolate)) {
-    frm <- frm[, !(colnames(frm) %in% mtp@misc$not_need_interpolate), drop = FALSE]
+    frm <- frm[, !(colnames(frm) %in% mtp@misc$not_need_interpolate), with = FALSE]
     if (any(mtp@misc$not_need_interpolate == 'year')) there.is.year <- FALSE
     fl <- add_set_name %in% mtp@misc$not_need_interpolate
     if (any(fl)) {
@@ -88,7 +86,7 @@ multiInterpolation <- function(frm, parameter, mtp, approxim,
                     year_range = range(approxim$year),
                     approxim   = approxim)
   if (is.null(dd)) return(NULL)
-  dd <- dd[dd[[ncol(dd)]] != 0 | dd$type == 'up',, drop = FALSE]
+  dd <- dd[dd[[ncol(dd)]] != 0 | dd$type == 'up',]
   if (nrow(dd) == 0) return(NULL)
   
   colnames(dd)[[ncol(dd)]] <- 'value'
@@ -97,7 +95,7 @@ multiInterpolation <- function(frm, parameter, mtp, approxim,
   }
   if (there.is.year) dd[['year']] <- as.numeric(dd[['year']])
   if (is.null(add_set_name)) {
-    dd <- dd[, c(mtp@dimSetNames, 'type', 'value'), drop = FALSE]
+    dd <- dd[, c(mtp@dimSetNames, 'type', 'value'), with = FALSE]
   } else {
     d3 <- data.table()
     for(i in 1:length(add_set_value))
@@ -110,18 +108,18 @@ multiInterpolation <- function(frm, parameter, mtp, approxim,
     } 
     stnd <- stnd[!(stnd %in% mtp@misc$not_need_interpolate)]
 
-    dd <- cbind(d3, dd[, c(stnd, 'type', 'value'), drop = FALSE])
+    dd <- cbind(d3, dd[, c(stnd, 'type', 'value'), with = FALSE])
   }
-  dd <- dd[(dd$type == 'lo') | (dd$type == 'up'),, drop = FALSE]
+  dd <- dd[(dd$type == 'lo') | (dd$type == 'up'),]
   if (!is.null(remove_duplicate) && nrow(dd) != 0) {
     fl <- rep(TRUE, nrow(dd))
     for (i in seq_along(remove_duplicate)) {
-      fl <- (fl & dd[, remove_duplicate[[i]][1]] != dd[, remove_duplicate[[i]][2]])
+      fl <- (fl & dd[, remove_duplicate[[i]][1], with = FALSE] != dd[, remove_duplicate[[i]][2], with = FALSE])
     }
-    dd <- dd[fl,, drop = FALSE]
+    dd <- dd[fl,]
   }
   if (there.is.year && !is.null(approxim$mileStoneYears)) {
-    dd <- dd[dd$year %in% approxim$mileStoneYears,, drop = FALSE]
+    dd <- dd[dd$year %in% approxim$mileStoneYears,]
   }
   if (nrow(dd) == 0) return(NULL)
   dd
