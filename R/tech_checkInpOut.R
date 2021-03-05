8e5b181efae71926faf4c6f5f97c1dfab17d0b6c
vpotashnikov <potashnikov.vu@gmail.com>
2021-03-05 21:07:02 +0300

    Update tech_checkInpOut.R

--- R/tech_checkInpOut.R
+++ R/tech_checkInpOut.R
@@ -81,43 +81,44 @@ checkInpOut <- function(tech) {
     aux[acomm_ind[i], 'output'] <- (any(!is.na(tech@aeff[tech@aeff$acomm == i, c('act2aout', 'cap2aout', 'ncap2aout'), with = FALSE])) || 
         any(!is.na(tech@aeff[tech@aeff$acomm == i, c('cinp2aout', 'cout2aout'), with = FALSE]))) 
   }
-  gtype <- data.table(type = factor(NULL, c('input', 'output')))
                       
   # Define type group
+  gtype <- data.table(type = factor(NULL, c('input', 'output')))
   group <- unique(c(tech@geff$group, tech@geff$geff, 
                     tech@group$group, tech@group$geff, 
                     tech@input$group, tech@output$group))
-  group <- group[!is.na(group)]
+  group <- unique(group[!is.na(group)])
   if (length(group) != 0) {
     if (any(is.na(c(tech@group$group, tech@group$geff)))) 
         stop('There is NA group in technology "', tech@name, '"')
-    
-  gtype[seq(along = group), ] <- NA
+
+   gtype <- data.table(group = group, type = factor(rep(NA, length(group)), c('input', 'output')))
+   
    for(i in unique(tech@geff$group)) {
      if (any(!is.na(tech@geff[tech@geff$group == i, 'ginp2use']))) {
-       if (!is.na(gtype[i, 'type']) && gtype[i, 'type'] == 'output') 
+       if (!is.na(gtype$type[i]) && gtype$type[i] == 'output') 
           stop('Wrong group in technology "', tech@name, '": "', i, '"')
-       gtype[i, 'type'] <- 'input'   
+       gtype$type[i] <- 'input'   
      }
    }
-   for(i in group) {
-     jj <- rownames(ctype)[!is.na(ctype$group) & ctype$group == i]
+   for(i in seq_along(group)) {
+     jj <- comm_ind[!is.na(ctype$group) & ctype$group == group[i]]
      if (length(jj) != 0) {
-       if (any(ctype[jj, 'type'] == 'input')) {
-         if (!is.na(gtype[i, 'type']) && gtype[i, 'type'] == 'output') 
+       if (any(ctype$type[jj] == 'input')) {
+         if (!is.na(gtype$type[i]) && gtype$type[i] == 'output') 
             stop('Wrong group in technology "', tech@name, '": "', i, '"')
-         gtype[i, 'type'] <- 'input'   
+         gtype$type[i] <- 'input'   
        }
        if (any(ctype[jj, 'type'] == 'output')) {
-         if (!is.na(gtype[i, 'type']) && gtype[i, 'type'] == 'input') 
+         if (!is.na(gtype$type[i]) && gtype$type[i] == 'input') 
             stop('Wrong group in technology "', tech@name, '": "', i, '"')
-         gtype[i, 'type'] <- 'output'   
+         gtype$type[i] <- 'output'   
        }
      }
    }
-   if (any(is.na(gtype[, 'type'])))
+   if (any(is.na(gtype$type)))
      stop('Wrong group in technology "', tech@name, '": "', 
-        paste(rownames(gtype)[is.na(gtype[, 'type'])], collapse = '", "'), '"')
+        paste(group[is.na(gtype$type)], collapse = '", "'), '"')
   }
   fcmd <- c(tech@ceff$comm, tech@aeff$comm[!is.na(tech@aeff$comm)], tech@aeff$acomm)
   fcmd <- fcmd[!(fcmd %in% c(tech@input$comm, tech@output$comm, tech@aux$acomm))]
