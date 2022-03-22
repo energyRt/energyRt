7e41fee81803ecd003ec96c19fab87d289eb81f7
olugovoy <olugovoy@gmail.com>
2022-03-22 18:30:51 -0400

    version stamp (see previous commits)

--- R/start_msg.R
+++ R/start_msg.R
@@ -1,4 +1,4 @@
 .onAttach <- function(...) {
-  packageStartupMessage("\n Loading package: energyRt \n Energy systems modeling toolbox in R \n Version: 0.01.21-dev (development version from GitHub) \n  <http://www.energyrt.org> \n ")
+  packageStartupMessage("\n Loading package: energyRt \n Energy systems modeling toolbox in R \n Version: 0.01.22-dev (development version from GitHub) \n  <http://www.energyrt.org> \n ")
 }
  
